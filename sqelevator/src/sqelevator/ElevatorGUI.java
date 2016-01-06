0e311d4d75d365bb179b08903412ab99401e5aae
BastiR1991 <basti.ratzenboeck@gmx.at>
2016-01-06 17:43:30 +0100

    updated ElevatorGUI

--- sqelevator/src/sqelevator/ElevatorGUI.java
+++ sqelevator/src/sqelevator/ElevatorGUI.java
@@ -2,11 +2,15 @@ package sqelevator;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+
+import javafx.scene.control.RadioButton;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
@@ -17,7 +21,12 @@ public class ElevatorGUI {
 	private static JSplitPane splitPane;
 	private static JPanel panelLeft;
 	private static JPanel panelRight;
-
+	
+	public static void addElevatorProgressBar(JPanel panel) {
+		JProgressBar elevatorProgressBar = new JProgressBar(JProgressBar.VERTICAL, 0, 100);
+		panel.add(elevatorProgressBar);
+	}
+	
 	public static void customizeFloor(JTextArea floor,
 			Dimension floorDimension, Font floorFont) {
 		floor.setPreferredSize(floorDimension);
@@ -34,6 +43,51 @@ public class ElevatorGUI {
 		upDownPanel.add(new JLabel("Down"));
 		panel.add(upDownPanel);
 	}
+	
+	public static void addDirectionInfo(JPanel panel) {
+		JPanel directionPanel = new JPanel();
+		Font directionFont = new Font("Verdana", Font.BOLD, 15);
+		JTextArea upDirection = new JTextArea("Up");
+		upDirection.setFont(directionFont);
+		JTextArea downDirection = new JTextArea("Down");
+		downDirection.setFont(directionFont);
+//		upDirection.setVisible(false);
+//		downDirection.setVisible(false);
+		directionPanel.add(upDirection);
+		directionPanel.add(downDirection);
+		panel.add(directionPanel);
+	}
+	
+	public static void addElevatorInfos(JPanel panel) {
+		JPanel elevatorInfos = new JPanel(new java.awt.GridLayout(4, 2));
+		JLabel payloadLabel = new JLabel("Payload:     ");
+		JLabel speedLabel = new JLabel("Speed:  ");
+		JLabel doorstatusLabel = new JLabel("Door Status:  ");
+		JLabel targetsLabel = new JLabel("Targets:  ");
+		JLabel payloadValueLabel = new JLabel("0");
+		JLabel speedValueLabel = new JLabel("0");
+		JLabel doorstatusValueLabel = new JLabel("0");
+		JLabel targetsValueLabel = new JLabel("0");
+		
+		elevatorInfos.add(payloadLabel);
+		elevatorInfos.add(payloadValueLabel);
+		elevatorInfos.add(speedLabel);
+		elevatorInfos.add(speedValueLabel);
+		elevatorInfos.add(doorstatusLabel);
+		elevatorInfos.add(doorstatusValueLabel);
+		elevatorInfos.add(targetsLabel);
+		elevatorInfos.add(targetsValueLabel);
+		panel.add(elevatorInfos);
+	}
+	
+	public static void addModePanel(JPanel panel) {
+		JPanel modePanel = new JPanel();
+		modePanel.add(new JLabel("Automatic Mode: "));
+		modePanel.add(new JRadioButton());
+		modePanel.add(new JLabel("Manual Mode:"));
+		modePanel.add(new JRadioButton());
+		panel.add(modePanel);
+	}
 
 	public static void main(String[] args) {
 		// create frame
@@ -46,13 +100,15 @@ public class ElevatorGUI {
 		// create panels with their subPanels
 		panelLeft = new JPanel();
 		panelLeft.setLayout(new javax.swing.BoxLayout(panelLeft, javax.swing.BoxLayout.Y_AXIS));
-		JPanel panelZero = new JPanel();
-		JPanel panelOne = new JPanel();
-		JPanel panelTwo = new JPanel();
-		JPanel panelThree = new JPanel();
-		JPanel panelFour = new JPanel();
+		// TODO
+		addElevatorProgressBar(panelLeft);
+		JPanel panelZero = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel panelOne = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel panelTwo = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel panelThree = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JPanel panelFour = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		panelRight = new JPanel();
-
+		
 		// create floors
 		Dimension floorDimension = new Dimension(50, 100);
 		Font floorFont = new Font("Verdana", Font.BOLD, 20);
@@ -73,13 +129,16 @@ public class ElevatorGUI {
 		panelTwo.add(floorTwo);
 		panelThree.add(floorThree);
 		panelFour.add(floorFour);
-		
+				
 		// add up and down to subPanels
 		addUpDownPanel(panelZero);
 		addUpDownPanel(panelOne);
 		addUpDownPanel(panelTwo);
 		addUpDownPanel(panelThree);
 		addUpDownPanel(panelFour);
+		
+		// add direction info
+		addDirectionInfo(panelZero);
 		
 		// add subPanels to panelLeft
 		panelLeft.add(panelZero);
@@ -88,8 +147,9 @@ public class ElevatorGUI {
 		panelLeft.add(panelThree);
 		panelLeft.add(panelFour);
 		
-		// TODO panelRight
-		panelRight.setBackground(Color.BLACK);
+		// add elevator infos and mode to panelRight
+		addElevatorInfos(panelRight);
+		addModePanel(panelRight);
 		
 		// add panel to splitPane
 		splitPane.setLeftComponent(panelLeft);
